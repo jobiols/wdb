d2155a76bfe2702a9c867d8e8c21211c8f3207b2
Jorge Obiols <jorge.obiols@gmail.com>
2025-06-22 16:02:04 -0300

    [FIX] compatiblidad con Tornado 6.0

--- test_debug.py
+++ test_debug.py
@@ -1,3 +1,11 @@
+
 import wdb
 wdb.set_trace()
+
+for a in range(10):
+    print(f"Iteración {a}")
+    if a == 5:
+        print("Punto de interrupción alcanzado")
+        wdb.set_trace()  # Aquí se detendrá el depurador
+        
 print("Termina el programa test_debug.py")
